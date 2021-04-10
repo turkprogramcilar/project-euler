8853aaf2eb15e6daff44124240999be72439ef75
Ertuğrul Dal <64368200+ertugruldal@users.noreply.github.com>
2021-04-10 11:56:30 +0300

    Create MrCHUCK#0799.cpp

---
+++ çözümler/0017/cpp/MrCHUCK#0799.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int one_nine = 36 * 90 ;
+    int ten_twenty = 70 * 10 ;
+    int hundred = 46 * 100 ;
+    int hundresss = 36 * 100  + 10 * 900  - 16 ;
+    std::cout << one_nine + ten_twenty + hundred + hundresss << std::endl;
+    return 0;
+}
