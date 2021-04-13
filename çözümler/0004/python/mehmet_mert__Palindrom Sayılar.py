9693c6af3cb8f504a4cfdf2ee9c4362f3d284113
mehmet-mert <mhmtmrtltnts@gmail.com>
2021-04-13 09:09:34 +0300

    2. ve 4. sorular eklendi

---
+++ çözümler/0004/python/mehmet_mert__Palindrom Sayılar.py
@@ -0,0 +1,25 @@
+#mehmet_mert__
+first_num = 999
+second_num = 999
+high_score = 0
+
+def reverse_slicing(s):
+    return s[::-1]
+
+while True:
+    if str(first_num*second_num) == reverse_slicing(str(first_num*second_num)):
+        if first_num*second_num > high_score:
+            high_score = first_num*second_num
+            print(first_num, "x", second_num)
+            print(high_score)
+        second_num -= 1
+    else:
+        second_num -= 1
+        if second_num < 100:
+            first_num -= 1
+            second_num = 999
+        if first_num < 100:
+            print("end")
+            break
+
+
