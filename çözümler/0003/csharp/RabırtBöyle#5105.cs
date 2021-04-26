cb1e7c5ba84980c736d052532a6ebd3c46fd4a19
hilmiAkkaya <74901189+hilmiAkkaya@users.noreply.github.com>
2021-04-26 13:47:43 +0300

    RabırtBöyle 3. Soru Çözümü

---
+++ çözümler/0003/csharp/RabırtBöyle#5105.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static bool IsPrime(long isPrimeNumber)
+        {
+            long j = 2;
+            long controller = 0;
+            while (j < isPrimeNumber)
+            {
+                if(isPrimeNumber%j == 0)
+                {
+                    controller++;
+                }
+                j++;
+            }
+            if(controller == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            long number = 600851475143;
+            long i = 1;
+            long lastPrime = 2;
+            while (i <= number)
+            {
+                if(number%i == 0)
+                {
+                    if (IsPrime(i))
+                    {
+                        lastPrime = i;
+                        Console.WriteLine(i);
+                    }
+                }
+                i++;
+            }
+            Console.WriteLine(lastPrime);
+            Console.ReadKey();
+        }
+    }
+}
