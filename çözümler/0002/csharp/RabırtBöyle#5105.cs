e0cb978509cafadafdad22cd9a3f727c5163d9a9
hilmiAkkaya <74901189+hilmiAkkaya@users.noreply.github.com>
2021-04-26 13:40:41 +0300

    RabırtBöyle 2. Soru Çözümü

---
+++ çözümler/0002/csharp/RabırtBöyle#5105.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 1, num2 = 2, num3 = num1 + num2, total = 2;
+            while(num3 <= 4000000)
+            {
+                num1 = num2; num2 = num3; num3 = num1 + num2;
+                if(num3%2 == 0)
+                {
+                    total+=num3;
+                }
+            }
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+    }
+}
